99b3e2166a1da3362eee07df54c41f99b37a5da5
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2025-03-26 13:52:23 +0530

    add login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import Form from "pages/Login/Form";
+
+function Login() {
+  const navigate = useNavigate();
+
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const login = async () => {
+    try {
+      if (!userData.email || !userData.password)
+        throw new Error("Email and password are required");
+
+      localStorage.setItem("token", true);
+      navigate("/home");
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) navigate("/home");
+  }, []);
+
+  return <Form {...{ userData, setUserData, login }} />;
+}
+
+export default Login;
