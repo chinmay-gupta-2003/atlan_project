ba71364830a734388585922c53c08a53622c2bc1
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2025-03-27 11:30:50 +0530

    add all line charts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,15 +5,21 @@ import { PulseLoader } from "react-spinners";
 import Table from "components/Table/Table";
 import { getTableData } from "utils/getTableData";
 import { columnsMapping } from "constants/mappings";
-import home from "assets/svg/home.svg";
 
+import home from "assets/svg/home.svg";
 import styles from "pages/Home/Home.module.css";
+
 import SimpleBarChart from "components/Charts/BarCharts/SimpleBarChart";
 import StackedBarChart from "components/Charts/BarCharts/StackedBarChart";
 import PositiveAndNegativeBarChart from "components/Charts/BarCharts/PositiveAndNegativeBarChart";
 import BarChartStackedBySign from "components/Charts/BarCharts/BarChartStackedBySign";
 import BiaxialBarChart from "components/Charts/BarCharts/BiaxialBarChart";
+import TinyBarChart from "components/Charts/BarCharts/TinyBarChart";
+
 import SimpleLineChart from "components/Charts/LineCharts/SimpleLineChart";
+import TinyLineChart from "components/Charts/LineCharts/TinyLineChart";
+import DashedLineChart from "components/Charts/LineCharts/DashedLineChart";
+import BiaxialLineChart from "components/Charts/LineCharts/BiaxialLineChart";
 
 function Home() {
   const { tableSelected, databaseSelected } = useSelector(
@@ -48,6 +54,10 @@ function Home() {
 
   const barCharts = [
     {
+      title: "Tiny Bar Chart",
+      component: <TinyBarChart />,
+    },
+    {
       title: "Simple Bar Chart",
       component: <SimpleBarChart />,
     },
@@ -71,8 +81,20 @@ function Home() {
 
   const lineCharts = [
     {
+      title: "Tiny Line Chart",
+      component: <TinyLineChart />,
+    },
+    {
       title: "Simple Line Chart",
       component: <SimpleLineChart />,
+    },
+    {
+      title: "Dashed Line Chart",
+      component: <DashedLineChart />,
+    },
+    {
+      title: "Biaxial Line Chart",
+      component: <BiaxialLineChart />,
     },
   ];
 
@@ -80,13 +102,13 @@ function Home() {
     <div className={styles.container}>
       <div className={styles.charts}>
         {barCharts.map((container) => (
-          <div className="chartWrapper">
+          <div className="chartWrapper" key={container.title}>
             <span>{container.title}</span>
             {container.component}
           </div>
         ))}
         {lineCharts.map((container) => (
-          <div className="chartWrapper">
+          <div className="chartWrapper" key={container.title}>
             <span>{container.title}</span>
             {container.component}
           </div>
